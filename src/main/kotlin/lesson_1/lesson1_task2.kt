3e1e634ae691a247b62173b5c701385433ae5f92
Andrey Yankov <ayankov@ozon.ru>
2024-05-10 16:26:47 +0300

    KS-1-2 Add variable for number of employees and print valuables of all variables to console.

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,10 @@
+package lesson_1
+fun main() {
+    val numberOfOrders: Int = 75
+    val thankYouForPurchaseText: String = "Спасибо за покупку!"
+    var numberOfEmployees: Int = 2000
+    println(numberOfOrders)
+    println(thankYouForPurchaseText)
+//    println(numberOfEmployees)
+    println(--numberOfEmployees)
+}
