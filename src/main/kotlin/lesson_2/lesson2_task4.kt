7eb066124b4fd8ba8b157452a61215e06b837322
Andrey Yankov <ayankov@ozon.ru>
2024-05-13 17:01:24 +0300

    KS-2-4 Add calculation of buff percentage

---
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -0,0 +1,10 @@
+package lesson_2
+
+fun main() {
+    val crystalOre = 7
+    val ironOre = 11
+    val buffPercentage = 20
+
+    println("Кристаллическая руда: ${crystalOre * buffPercentage / 100}")
+    print("Кристаллическая руда: ${ironOre * buffPercentage / 100}")
+}
