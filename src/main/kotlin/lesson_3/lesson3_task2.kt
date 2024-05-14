ac6d8e6b51cc95c0b2750c85997d73430aa82518
Andrey Yankov <ayankov@ozon.ru>
2024-05-14 11:57:51 +0300

    KS-3-2 Add variables for full name and print them

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,13 @@
+package lesson_3
+
+fun main() {
+    val surname = "Андреева"
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    val surnameAfterWedding = "Сидорова"
+    val age = 20
+    val ageOfChangeSurname = 22
+
+    println("$surname $name $patronymic $age")
+    println("$surnameAfterWedding $name $patronymic $ageOfChangeSurname")
+}
