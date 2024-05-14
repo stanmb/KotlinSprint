c1d33e63891357e3b3f0cbe0f185b1e7fff18b7a
Andrey Yankov <ayankov@ozon.ru>
2024-05-14 12:48:35 +0300

    KS-3-2 Add variable for timeDivider and use String.format for printing

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -4,11 +4,13 @@ fun main() {
     val departureTimeHours = 9
     val departureTimeMinutes = 39
     val travelTimeInMinutes = 457
+    val timeDivider = 60
 
-    val totalDepartureTimeInMinutes = departureTimeMinutes + (departureTimeHours * 60)
+    val totalDepartureTimeInMinutes = departureTimeMinutes + (departureTimeHours * timeDivider)
     val arrivalTimeInMinutes = travelTimeInMinutes + totalDepartureTimeInMinutes
-    val arrivalHours = arrivalTimeInMinutes / 60
-    val arrivalMinutes = arrivalTimeInMinutes % 60
+    val arrivalHours = arrivalTimeInMinutes / timeDivider
+    val arrivalMinutes = arrivalTimeInMinutes % timeDivider
 
-    println("$arrivalHours:$arrivalMinutes")
+    print(String.format("%02d", arrivalHours) + ":")
+    print(String.format("%02d", arrivalMinutes))
 }
