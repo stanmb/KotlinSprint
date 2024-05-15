b72c09ec913a8a6265d1fe7a74f93719a12a59a8
Andrey Yankov <ayankov@ozon.ru>
2024-05-15 17:33:08 +0300

    KS-3-5 Use vararg in  String.split()

--- src/main/kotlin/lesson3/lesson3_task5.kt
+++ src/main/kotlin/lesson3/lesson3_task5.kt
@@ -3,7 +3,7 @@ package lesson3
 fun main() {
     val movesString = "D2-D4;0"
     val from = movesString.split("-")[0]
-    val to = movesString.split(";")[0].split("-")[1]
+    val to = movesString.split(";", "-")[1]
     val moveNumber = movesString.split(";")[1]
 
     println(from)
