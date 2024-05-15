c4341d5a059e570fd94a09a01486b30ad15a678c
Andrey Yankov <ayankov@ozon.ru>
2024-05-15 17:21:00 +0300

    KS-2-4 Add variable for percentage base

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -4,7 +4,8 @@ fun main() {
     val crystalOre = 7
     val ironOre = 11
     val buffPercentage = 20
+    val percentageBase = 100
 
-    println("Кристаллическая руда: ${crystalOre * buffPercentage / 100}")
-    print("Кристаллическая руда: ${ironOre * buffPercentage / 100}")
+    println("Кристаллическая руда: ${crystalOre * buffPercentage / percentageBase}")
+    print("Кристаллическая руда: ${ironOre * buffPercentage / percentageBase}")
 }
