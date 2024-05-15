5e926fc0efb85dde3ab34db839c5de33bb618903
Andrey Yankov <ayankov@ozon.ru>
2024-05-15 17:29:52 +0300

    KS-2-5 Use String.format("%.3f") to limit digits after the dot

--- src/main/kotlin/lesson2/lesson2_task5.kt
+++ src/main/kotlin/lesson2/lesson2_task5.kt
@@ -1,7 +1,6 @@
 package lesson2
 
 import kotlin.math.pow
-import kotlin.math.roundToInt
 
 fun main() {
     val moneyAmount = 70000
@@ -9,6 +8,5 @@ fun main() {
     val periodOfDeposit = 20
 
     val finalMoneyAmount = moneyAmount * (1 + percentagePerYear / 100).pow(periodOfDeposit)
-    val finalMoneyAmountWithLimitedDigits = (finalMoneyAmount * 1000).roundToInt() / 1000.0
-    println(finalMoneyAmountWithLimitedDigits)
+    println(String.format("%.3f", finalMoneyAmount))
 }
