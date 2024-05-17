bf4c70779560cfcb12072bef1514da4929c2842a
Andrey Yankov <ayankov@ozon.ru>
2024-05-17 10:56:05 +0300

    KS-4-2 Move variables to constants

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,17 +1,18 @@
 package lesson_4
 
+const val CATEGORY = "Average"
+const val MIN_WEIGHT = 35
+const val MAX_WEIGHT = 100
+const val MAX_VOLUME = 100
+
 fun main() {
-    val category = "Average"
-    val minWeight = 35
-    val maxWeight = 100
-    val maxVolume = 100
     var weight = 20
     var volume = 80
 
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории '$category': ${ minWeight <= weight && weight <= maxWeight && volume < maxVolume}")
+    println("Груз с весом $weight кг и объемом $volume л соответствует категории '$CATEGORY': ${ MIN_WEIGHT <= weight && weight <= MAX_WEIGHT && volume < MAX_VOLUME}")
 
     weight = 50
     volume = 100
 
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории '$category': ${ minWeight <= weight && weight <= maxWeight && volume < maxVolume}")
+    println("Груз с весом $weight кг и объемом $volume л соответствует категории '$CATEGORY': ${ MIN_WEIGHT <= weight && weight <= MAX_WEIGHT && volume < MAX_VOLUME}")
 }
