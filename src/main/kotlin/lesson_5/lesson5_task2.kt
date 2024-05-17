81cc64577bfb96c6c59bdb695b577efd53995889
Andrey Yankov <ayankov@ozon.ru>
2024-05-17 11:59:21 +0300

    KS-5-2 Change example from the video, get year of birth instead of age

---
+++ src/main/kotlin/lesson_5/lesson5_task2.kt
@@ -0,0 +1,17 @@
+package lesson_5
+
+import java.time.LocalDate
+
+const val AGE_OF_MAJORITY = 18
+
+fun main() {
+    val yearOfUsersBirth = readlnOrNull()!!.toInt()
+    val currentYear = LocalDate.now().year
+    val userAge = currentYear - yearOfUsersBirth
+
+    val resultText = if (userAge >= AGE_OF_MAJORITY) "Показать экран со скрытым контентом"
+    else if (userAge == 16 || userAge == 17) "Показать экран с ограниченным контентом"
+    else "Вернуться на гланвый экран"
+
+    println(resultText)
+}
