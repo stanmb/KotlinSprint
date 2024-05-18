9e6c854b31f62949f5114a983a2d44130e0c7d88
Andrey Yankov <ayankov@ozon.ru>
2024-05-18 11:28:49 +0300

    KS-5-3 Add constants, variables, if clauses for the lottery

---
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -0,0 +1,28 @@
+package lesson_5
+
+const val NUM_ONE = 2
+const val NUM_TWO = 18
+
+fun main() {
+    var oneMatch = false
+    var twoMatches = false
+
+    println("Введите первое число")
+    val userNumOne = readln().toInt()
+    println("Введите второе число")
+    val userNumTwo = readln().toInt()
+
+    if (userNumOne == NUM_ONE || userNumOne == NUM_TWO) {
+        oneMatch = true
+        if (userNumTwo == NUM_ONE || userNumTwo == NUM_TWO) {
+            twoMatches = true
+        }
+    }
+    val resultString = if (oneMatch && !twoMatches) "Вы выиграли утешительный приз!"
+    else if (twoMatches) "Поздравляем! Вы выиграли главный приз!"
+    else "Неудача!"
+
+    println(resultString)
+
+    if (!oneMatch && !twoMatches) println("Числа которые нужно было угадать: $NUM_ONE, $NUM_TWO")
+}
